5020b8a86b8fb56c7ab968f07efbe76f1b9e3432
Punit Katiyar <punit.kat@gmail.com>
2022-11-08 14:19:49 +0530

    Add files via upload

---
+++ 06.model/Dbmaster.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class dbmaster extends CI_Model
+{
+    public function index()
+    {
+        echo "<h1>Hello I Am  DataBase Master</h1>";
+    }
+}
+
+
+?>
